acf36b7a275ff26d7bc085b379654a08c6eb971d
marek.dvoracek <marek.dvoracek606@gmail.com>
2025-12-04 19:52:43 +0100

    add fujtajbl

---
+++ Fujtajbl/Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace IPMasking
+{
+    public enum MathOperations : byte
+    {
+        Addition = 1,
+        Subtraction = 2,
+        Multiplication = 3,
+        Division = 4
+    }
+
+}
---
+++ Fujtajbl/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fujtajbl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RunApplication();
+            }
+            catch (OperationCanceledException)
+            {
+                SetColor(ConsoleColor.Yellow);
+                Console.WriteLine("\nApplication terminated by user.");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                SetColor(ConsoleColor.Red);
+                Console.WriteLine($"\nUnexpected error: {ex.Message}");
+                Console.ResetColor();
+            }
+
+            void RunApplication()
+            {
+                SetColor(ConsoleColor.Cyan);
+                Console.WriteLine("Welcome to Fujtajbl to VeryNice Calculator\n");
+                Console.ResetColor();
+
+
+
+            }
+
+            void SetColor(ConsoleColor color)
+            {
+                Console.ForegroundColor = color;
+            }
+
+            string GetAnswer(string prompt)
+            {
+                while (true)
+                {
+                    Console.WriteLine(prompt);
+                    var input = Console.ReadLine();
+
+                    if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                        throw new OperationCanceledException();
+
+                    if (!string.IsNullOrWhiteSpace(input))
+                        return input;
+
+                    SetColor(ConsoleColor.Red);
+                    Console.WriteLine("Invalid Input, try again.\n");
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
+}
