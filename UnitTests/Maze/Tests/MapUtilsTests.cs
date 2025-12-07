477064956f00e32ec54345841a411bb93ab4cacf
marek.dvoracek <marek.dvoracek606@gmail.com>
2025-12-07 09:17:26 +0100

    test maps

---
+++ UnitTests/Maze/Tests/MapUtilsTests.cs
@@ -0,0 +1,35 @@
+﻿using Maze.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maze.Core.Models;
+using UnitTests.Maze.BaseTest;
+
+namespace UnitTests.Maze.Tests
+{
+    public class MapUtilsTests : MazeBaseTests
+    {
+        [Fact]
+        public void LoadMapFromFile_LoadsCorrectly()
+        {
+            var path = GetMapPath("valid.dat");
+
+            var map = MapUtils.LoadMapFromFile(path);
+
+            Assert.NotEmpty(map);
+        }
+
+        [Fact]
+        public void GetStartPosition_ReturnsCorrectPoint()
+        {
+            var path = GetMapPath("valid.dat");
+            var map = MapUtils.LoadMapFromFile(path);
+
+            var start = MapUtils.GetStartPosition(map, Tiles);
+
+            Assert.Equal(new Point(1, 0), start);
+        }
+    }
+}
---
+++ UnitTests/Maze/Tests/MazeBaseTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maze.Core.Utils;
+
+namespace UnitTests.Maze.BaseTest
+{
+    public class MazeBaseTests
+    {
+        protected readonly Dictionary<MapTile, char> Tiles = new()
+        {
+            { MapTile.Start, 'S' },
+            { MapTile.End, 'E' },
+            { MapTile.Path, '.' },
+            { MapTile.Wall, '#' }
+        };
+
+        protected string GetMapPath(string fileName)
+        {
+            var baseDir = Directory.GetCurrentDirectory();
+
+            var projectDir = Path.GetFullPath(Path.Combine(baseDir, @"..\..\..\"));
+
+            return Path.Combine(projectDir, "Maze", "TestMaps", fileName);
+        }
+    }
+}
